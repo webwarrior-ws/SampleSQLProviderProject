7e7f5a145a67192f70c198fdbd68f903d2cd620a
webwarrior-ws <reg@webwarrior.ws>
2025-11-25 10:55:32 +0100

    Initial commit

---
+++ Program.fs
@@ -0,0 +1,19 @@
+﻿open System
+
+open FSharp.Data.Sql
+
+[<Literal>]
+let DevelopmentConnStr =
+    "Server=localhost;Port=5432;Database=test;User Id=postgres;Password='localDevPassword'"
+
+type SQL =
+    SqlDataProvider<Common.DatabaseProviderTypes.POSTGRESQL, DevelopmentConnStr, Owner="public, admin, references", UseOptionTypes=Common.NullableColumnType.OPTION, ResolutionPath="lib/">
+
+let ctx =
+    typeof<Npgsql.NpgsqlConnection>.Assembly |> ignore
+    SQL.GetDataContext DevelopmentConnStr
+
+let testRecord = ctx.Public.Test.Create()
+testRecord.String <- "test"
+testRecord.Datetime <- DateTime.Now
+ctx.SubmitUpdates()
